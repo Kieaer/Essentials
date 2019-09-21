b6acc23dddad4777ca696d5af0c535526441ddc0
키에르 <44261958+Kieaer@users.noreply.github.com>
2019-09-22 00:23:08 +0900

    Settings user-friendly improvement

--- src/main/java/essentials/EssentialConfig.java
+++ src/main/java/essentials/EssentialConfig.java
@@ -26,183 +26,159 @@ class EssentialConfig {
     static void main() {
         Map<String, Object> obj;
         if (!Core.settings.getDataDirectory().child("plugins/Essentials/config.txt").exists()) {
-            // Make file
-            Core.settings.getDataDirectory().child("plugins/Essentials/config.txt").writeString("");
+            String text =
+                   "# server to server chat config\n" +
+                    "server-enable: false\n" +
+                    "server-port: 25000\n\n" +
+                    "client-port: 20000\n" +
+                    "client-enable: false\n" +
+                    "client-host: localhost\n\n" +
 
-            DumperOptions options = new DumperOptions();
-            options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
-            options.setPrettyFlow(true);
-            Yaml yaml = new Yaml(options);
+                    "# If turn on realname, even if the player changes the nickname, it will be set to the previous nickname.\n" +
+                    "realname: true\n\n" +
 
-            Map<String, Object> data = new HashMap<>();
-            data.put("server-enable", false);
-            data.put("server-port", 25000);
-            data.put("client-port", 20000);
-            data.put("realname", true);
-            data.put("client-enable", false);
-            data.put("client-host", "localhost");
-            data.put("detectreactor", true);
-            data.put("basexp", 1000);
-            data.put("exponent", 1.12f);
+                    "# If turn on detectreactor, send alert message when the thorium reactor is overheated and explodes.\n" +
+                    "detectreactor: true\n\n" +
 
-            FileWriter writer = null;
-            try {
-                writer = new FileWriter(String.valueOf(Core.settings.getDataDirectory().child("plugins/Essentials/config.txt")));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            yaml.dump(data, writer);
+                    "# Experience value setting.\n# Base xp is required experience to level up from 1 to 2\n# exponent is EXP multiplier required for the next level.\n" +
+                    "basexp: 1000\n" +
+                    "exponent: 1.12f\n";
 
+            Core.settings.getDataDirectory().child("plugins/Essentials/config.txt").writeString(text);
             Log.info("[Essentials] config file created!");
         }
 
         if (!Core.settings.getDataDirectory().child("plugins/Essentials/Exp.txt").exists()) {
-            // Make file
-            Core.settings.getDataDirectory().child("plugins/Essentials/Exp.txt").writeString("");
+            String text =
+                    "# crafting\n" +
+                    "graphite-press: 5\n" +
+                    "multi-press: 15\n" +
+                    "silicon-smelter: 5\n" +
+                    "kiln: 8\n" +
+                    "plastanium-compressor: 20\n" +
+                    "phase-weaver: 35\n" +
+                    "cryofluidmixer: 15\n" +
+                    "blast-mixer: 8\n" +
+                    "pyratite-mixer: 12\n" +
+                    "melter: 3\n" +
+                    "separator: 6\n" +
+                    "spore-press: 5\n" +
+                    "pulverizer: 2\n" +
+                    "coal-centrifuge: 3\n" +
+                    "incinerator: 2\n\n" +
 
-            DumperOptions options = new DumperOptions();
-            options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
-            options.setPrettyFlow(true);
-            Yaml yaml = new Yaml(options);
+                    "# Defence\n" +
+                    "copper-wall: 1\n" +
+                    "copper-wall-large: 4\n" +
+                    "titanium-wall: 2\n" +
+                    "titanium-wall-large: 8\n" +
+                    "thorium-wall: 3\n" +
+                    "thorium-wall-large: 12\n" +
+                    "phase-wall: 4\n" +
+                    "phase-wall-large: 16\n" +
+                    "surge-wall: 5\n" +
+                    "surge-wall-large: 20\n" +
+                    "door: 2\n" +
+                    "door-large: 6\n" +
+                    "mender: 2\n" +
+                    "mend-projector: 10\n" +
+                    "overdrive-projector: 12\n" +
+                    "force-projector: 35\n" +
+                    "shock-mine: 5\n\n" +
 
-            Map<String, Object> data = new HashMap<>();
+                    "# Distribution\n" +
+                    "conveyor: 1\n" +
+                    "titanium-conveyor: 2\n" +
+                    "junction: 1\n" +
+                    "bridge-conveyor: 1\n" +
+                    "phase-conveyor: 5\n" +
+                    "sorter: 2\n" +
+                    "router: 1\n" +
+                    "distributor: 1\n" +
+                    "overflow-gate: 2\n" +
+                    "mass-driver: 15\n\n" +
 
-            // crafting
-            data.put("graphite-press", 5);
-            data.put("multi-press", 15);
-            data.put("silicon-smelter", 5);
-            data.put("kiln", 8);
-            data.put("plastanium-compressor", 20);
-            data.put("phase-weaver", 35);
-            data.put("cryofluidmixer", 15);
-            data.put("blast-mixer", 8);
-            data.put("pyratite-mixer", 12);
-            data.put("melter", 3);
-            data.put("separator", 6);
-            data.put("spore-press", 5);
-            data.put("pulverizer", 2);
-            data.put("coal-centrifuge", 3);
-            data.put("incinerator", 2);
+                    "# Liquid\n" +
+                    "mechanical-pump: 2\n" +
+                    "rotary-pump: 8\n" +
+                    "thermal-pump: 15\n" +
+                    "conduit: 2\n" +
+                    "pulse-conduit: 4\n" +
+                    "liquid-router: 3\n" +
+                    "liquid-tank: 10\n" +
+                    "liquid-junction: 2\n" +
+                    "bridge-conduit: 2\n" +
+                    "phase-conduit: 5\n\n" +
 
-            // defense
-            data.put("copper-wall", 1);
-            data.put("copper-wall-large", 4);
-            data.put("titanium-wall", 2);
-            data.put("titanium-wall-large", 8);
-            data.put("thorium-wall", 3);
-            data.put("thorium-wall-large", 12);
-            data.put("phase-wall", 4);
-            data.put("phase-wall-large", 16);
-            data.put("surge-wall", 5);
-            data.put("surge-wall-large", 20);
-            data.put("door", 2);
-            data.put("door-large", 6);
-            data.put("mender", 2);
-            data.put("mend-projector", 10);
-            data.put("overdrive-projector", 12);
-            data.put("force-projector", 35);
-            data.put("shock-mine", 5);
+                    "# Power\n" +
+                    "power-node: 1\n" +
+                    "power-node-large: 3\n" +
+                    "surge-tower: 10\n" +
+                    "battery: 3\n" +
+                    "battery-large: 21\n" +
+                    "combustion-generator: 5\n" +
+                    "thermal-generator: 8\n" +
+                    "turbine-generator: 12\n" +
+                    "rtg-generator: 15\n" +
+                    "solar-panel: 6\n" +
+                    "solar-panel-large: 18\n" +
+                    "thorium-reactor: 25\n" +
+                    "impact-reactor: 200\n\n" +
 
-            // Distribution
-            data.put("conveyor", 1);
-            data.put("titanium-conveyor", 2);
-            data.put("junction", 1);
-            data.put("bridge-conveyor", 1);
-            data.put("phase-conveyor", 5);
-            data.put("sorter", 2);
-            data.put("router", 1);
-            data.put("distributor", 1);
-            data.put("overflow-gate", 2);
-            data.put("mass-driver", 15);
+                    "# Production\n" +
+                    "mechanical-drill: 2\n" +
+                    "pneumatic-drill: 5\n" +
+                    "laser-drill: 10\n" +
+                    "blast-drill: 15\n" +
+                    "water-extractor: 3\n" +
+                    "cultivator: 7\n" +
+                    "oil-extractor: 15\n\n" +
 
-            // Liquid
-            data.put("mechanical-pump", 2);
-            data.put("rotary-pump", 8);
-            data.put("thermal-pump", 15);
-            data.put("conduit", 2);
-            data.put("pulse-conduit", 4);
-            data.put("liquid-router", 3);
-            data.put("liquid-tank", 10);
-            data.put("liquid-junction", 2);
-            data.put("bridge-conduit", 2);
-            data.put("phase-conduit", 5);
+                    "# Storage\n" +
+                    "vault: 20\n" +
+                    "container: 7\n" +
+                    "unloader: 3\n\n" +
 
-            // Power
-            data.put("power-node", 1);
-            data.put("power-node-large", 3);
-            data.put("surge-tower", 10);
-            data.put("battery", 3);
-            data.put("battery-large", 21);
-            data.put("combustion-generator", 5);
-            data.put("thermal-generator", 8);
-            data.put("turbine-generator", 12);
-            data.put("rtg-generator", 15);
-            data.put("solar-panel", 6);
-            data.put("solar-panel-large", 18);
-            data.put("thorium-reactor", 25);
-            data.put("impact-reactor", 200);
+                    "# Turrets\n" +
+                    "duo: 3\n" +
+                    "scatter: 5\n" +
+                    "scorch: 8\n" +
+                    "hail: 10\n" +
+                    "wave: 12\n" +
+                    "lancer: 15\n" +
+                    "arc: 18\n" +
+                    "swarmer: 30\n" +
+                    "salvo: 24\n" +
+                    "fuse: 40\n" +
+                    "ripple: 50\n" +
+                    "cyclone: 60\n" +
+                    "spectre: 100\n" +
+                    "meltdown: 250\n\n" +
 
-            // Production
-            data.put("mechanical-drill", 2);
-            data.put("pneumatic-drill", 5);
-            data.put("laser-drill", 10);
-            data.put("blast-drill", 15);
-            data.put("water-extractor", 3);
-            data.put("cultivator", 7);
-            data.put("oil-extractor", 15);
+                    "# Units\n" +
+                    "draug-factory: 8\n" +
+                    "spirit-factory: 12\n" +
+                    "phantom-factory: 18\n" +
+                    "command-center: 30\n" +
+                    "wraith-factory: 15\n" +
+                    "ghoul-factory: 25\n" +
+                    "revenant-factory: 40\n" +
+                    "dagger-factory: 6\n" +
+                    "crawler-factory: 8\n" +
+                    "titan-factory: 12\n" +
+                    "fortress-factory: 18\n" +
+                    "repair-point: 2\n\n" +
 
-            // Storage
-            data.put("vault", 20);
-            data.put("container", 7);
-            data.put("unloader", 3);
+                    "# Upgrade\n" +
+                    "dart-mech-pad: 15\n" +
+                    "delta-mech-pad: 30\n" +
+                    "tau-mech-pad: 40\n" +
+                    "omega-mech-pad: 150\n" +
+                    "javelin-ship-pad: 35\n" +
+                    "trident-ship-pad: 45\n" +
+                    "glaive-ship-pad: 150";
 
-            // Turrets
-            data.put("duo", 3);
-            data.put("scatter", 5);
-            data.put("scorch", 8);
-            data.put("hail", 10);
-            data.put("wave", 12);
-            data.put("lancer", 15);
-            data.put("arc", 18);
-            data.put("swarmer", 30);
-            data.put("salvo", 24);
-            data.put("fuse", 40);
-            data.put("ripple", 50);
-            data.put("cyclone", 60);
-            data.put("spectre", 100);
-            data.put("meltdown", 250);
-
-            // Units
-            data.put("draug-factory", 8);
-            data.put("spirit-factory", 12);
-            data.put("phantom-factory", 18);
-            data.put("command-center", 30);
-            data.put("wraith-factory", 15);
-            data.put("ghoul-factory", 25);
-            data.put("revenant-factory", 40);
-            data.put("dagger-factory", 6);
-            data.put("crawler-factory", 8);
-            data.put("titan-factory", 12);
-            data.put("fortress-factory", 18);
-            data.put("repair-point", 2);
-
-            // Upgrade
-            data.put("dart-mech-pad", 15);
-            data.put("delta-mech-pad", 30);
-            data.put("tau-mech-pad", 40);
-            data.put("omega-mech-pad", 150);
-            data.put("javelin-ship-pad", 35);
-            data.put("trident-ship-pad", 45);
-            data.put("glaive-ship-pad", 150);
-
-            FileWriter writer = null;
-            try {
-                writer = new FileWriter(String.valueOf(Core.settings.getDataDirectory().child("plugins/Essentials/Exp.txt")));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            yaml.dump(data, writer);
-
+            Core.settings.getDataDirectory().child("plugins/Essentials/Exp.txt").writeString(text);
             Log.info("[Essentials] Exp config file created!");
         }
 
