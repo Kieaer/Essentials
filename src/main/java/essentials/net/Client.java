70fbbb3399d0356ccc034f65eaf3389883620e2b
키에르 <44261958+Kieaer@users.noreply.github.com>
2019-11-02 16:56:22 +0900

    Hot fix version 5.1.3

--- src/main/java/essentials/net/Client.java
+++ src/main/java/essentials/net/Client.java
@@ -60,7 +60,7 @@ public class Client extends Thread{
             JSONObject object = new JSONObject(parser);
 
             DefaultArtifactVersion latest = new DefaultArtifactVersion(object.getString("tag_name"));
-            DefaultArtifactVersion current = new DefaultArtifactVersion("5.1.2");
+            DefaultArtifactVersion current = new DefaultArtifactVersion("5.1.3");
 
             if(latest.compareTo(current) > 0){
                 Global.log("New version found!");
