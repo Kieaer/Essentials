9eae5abdf5258527db7a6e6af0c593c95d6991d4
Kieaer <kieaer@naver.com>
2019-12-08 16:02:42 +0900

    Log optimizing

--- src/main/java/essentials/core/Log.java
+++ src/main/java/essentials/core/Log.java
@@ -17,8 +17,8 @@ import java.time.format.DateTimeFormatter;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import static essentials.Global.config;
 import static essentials.Global.getTime;
+import static essentials.Global.nbundle;
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 public class Log{
@@ -27,105 +27,38 @@ public class Log{
     public void main() {
         // No error, griefer, non-block, withdraw event
         Events.on(PlayerChatEvent.class, e -> {
-            writelog("chat", getTime() + e.player.name + ": " + e.message);
+            writelog("chat", e.player.name + ": " + e.message);
         });
-
-        /*Events.on(WorldLoadEvent.class, e -> {
-            Path total = Paths.get(String.valueOf(Core.settings.getDataDirectory().child("mods/Essentials/Logs/Total.log")));
-            try {
-                String text = gettime() + "World loaded!\n";
-                byte[] result = text.getBytes();
-                Files.write(total, result, StandardOpenOption.APPEND);
-            } catch (IOException error) {
-                printStackTrace(error);
-            }
-        });*/
 
         Events.on(TapEvent.class, e-> {
             if (e.tile.entity != null && e.tile.entity.block != null && e.tile.entity() != null && e.player != null && e.player.name != null) {
-                if (config.getLanguage().equals("ko")) {
-                    writelog("tap", getTime() + e.player.name + " 플레이어가 " + e.tile.entity.block.name + " 블럭을 건드렸습니다.");
-                } else {
-                    writelog("tap", getTime() + "Player "+e.player.name + " has touched " + e.tile.entity.block.name + ".");
-                }
+                writelog("tap", nbundle("log-tap", e.player.name, e.tile.entity.block.name));
             }
         });
 
         Events.on(TapConfigEvent.class, e-> {
             if (e.tile.entity != null && e.tile.entity.block != null && e.tile.entity() != null && e.player != null && e.player.name != null) {
-                if (config.getLanguage().equals("ko")) {
-                    writelog("tap", getTime() + e.player.name + " 플레이어가 " + e.tile.entity.block.name + " 블럭의 설정을 변경했습니다.");
-                } else {
-                    writelog("tap", getTime() + "Player "+e.player.name + " edited " + e.tile.entity.block.name + "'s block setting.");
-                }
+                writelog("tap", nbundle("log-tap-config", e.player.name, e.tile.entity.block.name));
             }
         });
 
         Events.on(BlockBuildEndEvent.class, e -> {
             if(!e.breaking && e.tile.entity() != null && e.player != null && e.tile.entity.block != null && e.player.name != null) {
-                if (config.getLanguage().equals("ko")) {
-                    writelog("block", getTime() + e.player.name + " 플레이어가 " + e.tile.entity.block.name + " 블럭을 만들었습니다.");
-                } else {
-                    writelog("block", getTime() + "Player "+e.player.name + " has created block " + e.tile.entity.block.name + ".");
-                }
+                writelog("block", nbundle("log-block-place", e.player.name, e.tile.entity.block.name));
             }
         });
 
         Events.on(BuildSelectEvent.class, e -> {
             if(e.breaking && e.builder instanceof Player && e.builder.buildRequest() != null && !e.builder.buildRequest().block.name.matches(".*build.*")) {
-                if (config.getLanguage().equals("ko")) {
-                    writelog("block", getTime() + ((Player) e.builder).name + " 플레이어가 " + e.builder.buildRequest().block.name + " 블럭을 파괴했습니다.");
-                } else {
-                    writelog("block", getTime() + "Player "+((Player) e.builder).name + " has destroyed block " + e.builder.buildRequest().block.name + ".");
-                }
+                writelog("block", nbundle("log-block-remove", ((Player) e.builder).name, e.builder.buildRequest().block.name));
             }
         });
 
-        Events.on(MechChangeEvent.class, e -> {
-            if(config.getLanguage().equals("ko")) {
-                writelog("player", getTime() + e.player.name + " 플레이어가 기체를 " + e.mech.name + " 으로 변경했습니다.");
-            } else {
-                writelog("player", getTime() + "Player "+e.player.name + " has change mech to " + e.mech.name + ".");
-            }
-        });
-
-        Events.on(PlayerJoin.class, e -> {
-            String ip = Vars.netServer.admins.getInfo(e.player.uuid).lastIP;
-
-            if(config.getLanguage().equals("ko")) {
-                writelog("player", getTime() + e.player.name + "/" + e.player.uuid + "/" + ip + " 플레이어가 서버에 입장했습니다.");
-            } else {
-                writelog("player", getTime() + "Player "+e.player.name + "/" + e.player.uuid + "/" + ip + " joined.");
-            }
-        });
-
-        Events.on(PlayerConnect.class, e -> {
-            String ip = Vars.netServer.admins.getInfo(e.player.uuid).lastIP;
-
-            if(config.getLanguage().equals("ko")) {
-                writelog("player", getTime() + e.player.name + "/" + e.player.uuid + "/" + ip + " 플레이어가 서버에 연결했습니다.");
-            } else {
-                writelog("player", getTime() + "Player "+e.player.name + "/" + e.player.uuid + "/" + ip + " connected.");
-            }
-        });
-
-        Events.on(PlayerLeave.class, e -> {
-            String ip = Vars.netServer.admins.getInfo(e.player.uuid).lastIP;
-
-            if(config.getLanguage().equals("ko")) {
-                writelog("player", getTime() + e.player.name + "/" + e.player.uuid + "/" + ip + " 플레이어가 서버에서 나갔습니다.");
-            } else {
-                writelog("player", getTime() + "Player "+ e.player.name + "/" + e.player.uuid + "/" + ip + " disconnected.");
-            }
-        });
-
-        Events.on(DepositEvent.class, e -> {
-            if(config.getLanguage().equals("ko")) {
-                writelog("deposit", getTime() + e.player.name + " 플레이어가 " + e.player.item().item.name + " 아이템을 " + e.tile.block().name + " 으로 직접 옮겼습니다.");
-            } else {
-                writelog("deposit", getTime() + "Player "+ e.player.name + " has moved item " + e.player.item().item.name + " to " + e.tile.block().name + ".");
-            }
-        });
+        Events.on(MechChangeEvent.class, e -> writelog("player", nbundle("log-mech-change", e.player.name, e.mech.name)));
+        Events.on(PlayerJoin.class, e -> writelog("player", nbundle("log-player-join", e.player.name, e.player.uuid, Vars.netServer.admins.getInfo(e.player.uuid).lastIP)));
+        Events.on(PlayerConnect.class, e -> writelog("player", nbundle("log-player-connect", e.player.name, e.player.uuid, Vars.netServer.admins.getInfo(e.player.uuid).lastIP)));
+        Events.on(PlayerLeave.class, e -> writelog("player", nbundle("log-player-leave", e.player.name, e.player.uuid, Vars.netServer.admins.getInfo(e.player.uuid).lastIP)));
+        Events.on(DepositEvent.class, e -> writelog("deposit", nbundle("log-deposit", e.player.name, e.player.item().item.name, e.tile.block().name)));
     }
 
     public static void writelog(String type, String text){
@@ -137,12 +70,7 @@ public class Log{
             FileHandle logfolder = Core.settings.getDataDirectory().child("mods/Essentials/log");
 
             if (mainlog != null && mainlog.length() > 1024 * 512) {
-                if (config.getLanguage().equals("ko")) {
-                    mainlog.writeString("== 이 파일의 끝입니다. 날짜: " + date, true);
-                } else {
-                    mainlog.writeString("== End of log file. Date: " + date, true);
-                }
-
+                mainlog.writeString(nbundle("log-file-end", date), true);
                 try {
                     Files.move(newlog, oldlog, REPLACE_EXISTING);
                 } catch (IOException e) {
@@ -155,7 +83,7 @@ public class Log{
                 mainlog = logfolder.child(type + ".log");
             }
 
-            mainlog.writeString(text + "\n", true);
+            mainlog.writeString(getTime() + text + "\n", true);
         });
         ex.submit(t);
     }
