bc9e855d631cacb7c25556358d6ce51b80126630
Kieaer <kieaer@naver.com>
2019-12-22 12:12:10 +0900

    Fix https://github.com/Kieaer/Essentials/issues/39

--- src/main/java/essentials/core/EPG.java
+++ src/main/java/essentials/core/EPG.java
@@ -26,7 +26,7 @@ public class EPG {
                     String name = e.tile.block().name;
                     int level = (int) db.get("level");
                     Yaml yaml = new Yaml();
-                    Map<String, Object> obj = yaml.load(String.valueOf(Core.settings.getDataDirectory().child("mods/Essentials/BlockReqExp.txt").readString()));
+                    Map<String, Object> obj = yaml.load(String.valueOf(Core.settings.getDataDirectory().child("mods/Essentials/BlockReqExp.yml").readString()));
                     int blockreqlevel = 100;
                     if(String.valueOf(obj.get(name)) != null) {
                         try{
