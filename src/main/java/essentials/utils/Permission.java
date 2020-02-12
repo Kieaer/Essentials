8497c3fef30dde7032e99ab5452bb1e7fcd46f05
Kieaer <kieaer@naver.com>
2020-02-12 22:30:57 +0900

    Remove permission debug message

--- src/main/java/essentials/utils/Permission.java
+++ src/main/java/essentials/utils/Permission.java
@@ -3,7 +3,6 @@ package essentials.utils;
 import org.hjson.JsonObject;
 import org.hjson.JsonValue;
 
-import static essentials.Global.nlog;
 import static essentials.Global.printError;
 import static essentials.Main.root;
 
@@ -16,14 +15,11 @@ public class Permission {
                 permission = JsonValue.readHjson(root.child("permission.hjson").reader()).asObject();
                 for (JsonObject.Member data : permission) {
                     String name = data.getName();
-                    nlog("debug", "target: " + name);
                     if(permission.get(name).asObject().get("inheritance") != null) {
                         String inheritance = permission.get(name).asObject().getString("inheritance", null);
-                        nlog("debug", "target inheritance: " + name + "/" + inheritance);
                         while (inheritance != null) {
                             for (int a = 0; a < permission.get(inheritance).asObject().get("permission").asArray().size(); a++) {
                                 permission.get(name).asObject().get("permission").asArray().add(permission.get(inheritance).asObject().get("permission").asArray().get(a).asString());
-                                nlog("debug", "target inheritance add: " + name + "/" + inheritance + "/" + permission.get(inheritance).asObject().get("permission").asArray().get(a).asString());
                             }
                             inheritance = permission.get(inheritance).asObject().getString("inheritance", null);
                         }
