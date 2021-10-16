c6e515ac7e910bf9fca6c9d0547de3c29d5433e2
kieaer <kieaer@naver.com>
2021-10-17 00:26:00 +0900

    Some changes

---
+++ src/main/kotlin/remake/FileWatchService.kt
@@ -0,0 +1,45 @@
+package remake
+
+import arc.Core
+import arc.files.Fi
+import arc.util.Log
+import arc.util.async.Threads.sleep
+import java.io.IOException
+import java.nio.file.*
+
+object FileWatchService : Runnable {
+    private val root: Fi = Core.settings.dataDirectory.child("mods/Essentials/")
+    private var watchService: WatchService = FileSystems.getDefault().newWatchService()
+
+    override fun run() {
+        var watchKey: WatchKey
+        val path = Paths.get(root.absolutePath())
+        path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.OVERFLOW)
+
+        while(!Thread.currentThread().isInterrupted) {
+            try {
+                watchKey = watchService.take()
+                sleep(50)
+                val events = watchKey.pollEvents()
+                for(event in events) {
+                    val kind = event.kind()
+                    val paths = (event.context() as Path).fileName.toString()
+                    if((paths == "permission_user.hjson" || paths == "permission.hjson") && kind == StandardWatchEventKinds.ENTRY_MODIFY) {
+                        Permission.read()
+                        Log.info("Permission file updated!")
+                    }
+                }
+                if(!watchKey.reset()) {
+                    try {
+                        watchService.close()
+                        break
+                    } catch(e: IOException) {
+                        println(e)
+                    }
+                }
+            } catch(e: InterruptedException) {
+                Thread.currentThread().interrupt()
+            }
+        }
+    }
+}
--- src/main/kotlin/remake/Trigger.kt
+++ src/main/kotlin/remake/Trigger.kt
@@ -1,10 +1,13 @@
 package remake
 
+import mindustry.gen.Playerc
+
 class Trigger {
     fun load(){
 
     }
 
-
-
+    fun loadPlayer(player: Playerc, data: DB.PlayerData){
+        player.name(data.name)
+    }
 }
