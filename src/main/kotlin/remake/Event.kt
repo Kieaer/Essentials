e2e126c4d3d11d8946d2a1e79b3bf0e22a064fcd
Kieaer <cloud9350@naver.com>
2022-01-08 20:33:55 +0900

    Log

--- src/main/kotlin/remake/Event.kt
+++ src/main/kotlin/remake/Event.kt
@@ -1,25 +1,41 @@
 package remake
 
 import arc.Events
+import arc.util.Log
 import mindustry.Vars.netServer
 import mindustry.game.EventType
 import mindustry.game.EventType.ConfigEvent
 import mindustry.gen.Call
+import remake.DB.root
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardCopyOption
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 object Event {
     fun register(){
         Events.on(EventType.PlayerChatEvent::class.java){
-            if (netServer.chatFormatter.format(it.player, it.player.name) != null) {
+            if (!it.message.startsWith("/")) {
+                log(LogType.Chat, "${it.player.name}: ${it.message}")
+                Log.info("<&y" + it.player.name + ": &lm" + it.message + "&lg>")
+            }
+
+            // todo 채팅 포맷 변경
+            if (netServer.chatFormatter.format(it.player, it.message) != null) {
                 Call.sendMessage(
                     Permission.data[it.player.id].asObject()["chatFormat"].asString()
-                        .replace("%1", netServer.chatFormatter.format(it.player, it.player.name))
+                        .replace("%1", netServer.chatFormatter.format(it.player, it.message))
                         .replace("%2", it.message)
                 )
             }
         }
 
         Events.on(EventType.WithdrawEvent::class.java){
-
+            if (it.tile != null && it.player.unit().item() != null && it.player.name != null) {
+                log(LogType.WithDraw, "${it.player.name} puts ${it.player.unit().item().name} ${it.amount.toString()} amount into ${it.tile.block().name}.")
+            }
         }
 
         Events.on(EventType.DepositEvent::class.java){
@@ -109,5 +125,32 @@ object Event {
         Events.run(EventType.Trigger.impactPower){
 
         }
+    }
+
+    fun log(type: LogType, text: String) {
+        val date = DateTimeFormatter.ofPattern("yyyy-MM-dd HH_mm_ss").format(LocalDateTime.now())
+        val new = Paths.get(root.child("log/$type.log").path())
+        val old = Paths.get(root.child("log/old/$type/$date.log").path())
+        var main = root.child("log/$type.log")
+        val folder = root.child("log")
+
+        if(main != null && main.length() > 2048 * 256) {
+            main.writeString("end of file. $date", true)
+            try {
+                if(!root.child("log/old/$type").exists()) {
+                    root.child("log/old/$type").mkdirs()
+                }
+                Files.move(new, old, StandardCopyOption.REPLACE_EXISTING)
+            } catch(e: IOException) {
+                e.printStackTrace()
+            }
+            main = null
+        }
+        if(main == null) main = folder.child("$type.log")
+        main!!.writeString("[${LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"))}] $text\n", true)
+    }
+
+    enum class LogType {
+        Log, Warn, Error, Debug, Server, ServerWarn, ServerError, Client, ClientWarn, ClientError, Config, Player, PlayerWarn, PlayerError, Tap, WithDraw, Block, Deposit, Chat, Griefer, Web
     }
 }
