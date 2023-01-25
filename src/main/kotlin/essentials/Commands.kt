72629d956e22954846cf7497809fb0da6d43d90f
Gureumi <cloud9350@naver.com>
2023-01-26 03:37:43 +0900

    Add fake command (New modified client?)

--- src/main/kotlin/essentials/Commands.kt
+++ src/main/kotlin/essentials/Commands.kt
@@ -108,6 +108,7 @@ class Commands(handler: CommandHandler, isClient: Boolean) {
             handler.register("spawn", "<unit/block> <name> [amount/rotate]", "Spawns units at the player's location.") { a, p: Playerc -> Client(a, p).spawn() }
             handler.register("status", "Show server status") { a, p: Playerc -> Client(a, p).status() }
             handler.register("t", "<message...>", "Send a message only to your teammates.") { a, p: Playerc -> Client(a, p).t() }
+            handler.register("test", "<any...>", "Fake command") { _, p: Playerc -> p.kick(Packets.KickReason.customClient)}
             handler.register("team", "<team_name> [name]", "Change team") { a, p: Playerc -> Client(a, p).team() }
             handler.register("tempban", "<player> <time> [reason]", "Ban the player for a certain period of time.") { a, p: Playerc -> Client(a, p).tempban() }
             handler.register("time", "Show server time") { a, p: Playerc -> Client(a, p).time() }
